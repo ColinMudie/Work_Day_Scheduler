1c97ecadb1f18ada71aa4dd92aacac758b8ea81f
Colin Mudie <csmudie1@gmail.com>
2021-01-05 19:53:13 -0500

    created a system to check and display if the time timeblocks are past, present, or future

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 
 // current time displayed as "Day of the week, Month Day, Year 6:30 PM"
 let m = moment().format('LLLL');
-let now = moment()
+let now = moment();
+let nowHour = moment().format('H');
 let eight = 8;
 console.log(m);
-let possibleTime = ['9 AM','10 AM','11 AM','12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM']
+console.log(nowHour);
+let possibleTime = [9,10,11,12,1,2,3,4,5];
+let possibleTime24hr = [9,10,11,12,13,14,15,16,17];
 // displays current day to Jumbotron
-$('#currentDay').append(m)
+$('#currentDay').append(m);
 
 
-// for (let index = 0; index < array.length; index++) {
+
 
 
 for (let index = 0; index < possibleTime.length; index++) {
-    let hourId = possibleTime[index]    
+    let ampm = '';
+    if(possibleTime[index] > 8 & possibleTime[index] < 12){
+        ampm = 'am';
+    } else {
+        ampm = 'pm';
+    }
+    let hourId = possibleTime[index] + ampm;
+
+    // variable to compare current time to the 9-5 to determine what class the textarea should be
+    let textAreaId = '';
+    if (possibleTime24hr[index] < nowHour){
+        textAreaId = 'past';
+    } else if (possibleTime24hr[index] === nowHour){
+        textAreaId = 'present';
+    } else {
+        textAreaId = 'future';
+    }
+
+    console.log(hourId + textAreaId);
+    // html format of of row with time, text area, and save button.
     let timeBlocks = $(`
     <div class="row">
         <div class = "col-2 hour" id = ${hourId}>
             ${hourId}
             </div>
-        <textarea class = "col-8 textarea"></textarea>
+        <textarea class = "col-8 ${textAreaId}"></textarea>
         <div class = "col-2 saveBtn">
             <i class = "fas fa-save"> Save</i>
         </div>
